e9b9af58fa94e8a2ff1603c87427eaee8cc0e776
WillingHsieh <willinghsieh@me.com>
2024-02-18 23:54:34 +0800

    Branch_sub1/sub2_1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get( "/")
 def read_root():
-    return { "Hello": "瘋狂的FastAPI, 這里是分支 ADD"}
+    return { "Hello": "瘋狂的FastAPI, 這里是分支 sub1/sub2_1"}
 
 @app.get( "/blog/{id}")
 def get_blog( id: int):
