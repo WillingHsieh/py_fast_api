fb13b2a558ba234acffd0c2fe20afcef88d26ab4
WillingHsieh <willinghsieh@me.com>
2024-02-18 23:35:34 +0800

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+# print( "begin...")
+app = FastAPI()
+
+@app.get( "/")
+def read_root():
+    return { "Hello": "瘋狂的FastAPI, 這里是分支master"}
+
+@app.get( "/blog/{id}")
+def get_blog( id: int):
+    return { "data": f"Blog id:{id}"}
